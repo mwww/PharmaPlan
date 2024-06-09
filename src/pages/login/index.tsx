dbce0ba5faef927b608e4d2cc27311710edbbfea
Dominikus Sebastian Ramli <sebastianramli@rocketmail.com>
2024-06-09 16:09:13 +0700

    Added email input validation in Login Page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent, KeyboardEvent } from 'react';
+import { useState, ChangeEvent, KeyboardEvent, useEffect } from 'react';
 
 import setTitle from "../../helper/title/title";
 import css from './.module.scss';
@@ -7,13 +7,38 @@ import Modal from './modal/modal';
 
 interface LoginProps {
     onLogin: () => void; 
-  }
+}
 
-export default function Login({onLogin} : LoginProps) {
+export default function Login({ onLogin }: LoginProps) {
     const [email, setEmail] = useState<string>('');
+    const [emailError, setEmailError] = useState<string>('');
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
+    const [debouncedEmail, setDebouncedEmail] = useState<string>(email);
+    
     setTitle("Login");
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedEmail(email);
+        }, 300); // 500ms debounce delay
+
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [email]);
+
+    useEffect(() => {
+        if (debouncedEmail) {
+            const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+            if (!emailPattern.test(debouncedEmail)) {
+                setEmailError('Please enter a valid email address');
+            } else {
+                setEmailError('');
+            }
+        } else {
+            setEmailError('');
+        }
+    }, [debouncedEmail]);
 
     const handleKeyDown = (e: KeyboardEvent) => {
         if (e.key === 'Enter') {
@@ -26,7 +51,17 @@ export default function Login({onLogin} : LoginProps) {
     };
 
     const handleSubmit = () => {
-        console.log("Submitted email:", email);
+        if (!emailError && email) {
+            console.log("Submitted email:", email);
+            setIsModalOpen(true);
+            onLogin();
+        } else {
+            setEmailError('Please enter a valid email address');
+        }
+    };
+
+    const handleProviderLogin = (provider: string) => {
+        console.log(`Logging in with ${provider}`);
         setIsModalOpen(true);
         onLogin();
     };
@@ -58,12 +93,31 @@ export default function Login({onLogin} : LoginProps) {
                     onKeyDown={handleKeyDown}
                     required
                 />
-                <img src={next} alt="" onClick={handleSubmit} style={{ cursor: 'pointer' }} />
-                <p>Or use one of the following provider</p>
+                {emailError && <p className={css.error}>{emailError}</p>}
+                <img src={next} alt="" onClick={handleSubmit} style={{ 
+                        cursor: emailError ? 'not-allowed' : 'pointer', 
+                        opacity: emailError ? 0.5 : 1 
+                    }}  />
+                <p>Or use one of the following providers</p>
                 <div className={css.icons}>
-                    <img src="./assets/Login/icons/Google.png" alt="Google" width="25" onClick={handleSubmit}/>
-                    <img src="./assets/Login/icons/Meta.png" alt="Meta" width="25" onClick={handleSubmit}/>
-                    <img src="./assets/Login/icons/Microsoft.png" alt="Microsoft" width="25" onClick={handleSubmit}/>
+                    <img 
+                        src="./assets/Login/icons/Google.png" 
+                        alt="Google" 
+                        width="25" 
+                        onClick={() => handleProviderLogin('Google')} 
+                    />
+                    <img 
+                        src="./assets/Login/icons/Meta.png" 
+                        alt="Meta" 
+                        width="25" 
+                        onClick={() => handleProviderLogin('Meta')} 
+                    />
+                    <img 
+                        src="./assets/Login/icons/Microsoft.png" 
+                        alt="Microsoft" 
+                        width="25" 
+                        onClick={() => handleProviderLogin('Microsoft')} 
+                    />
                 </div>
             </div>
 
@@ -75,4 +129,3 @@ export default function Login({onLogin} : LoginProps) {
         </div>
     );
 }
-
