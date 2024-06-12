41e950ab658610ba12680549604979ccb6489b89
Dominikus Sebastian Ramli <sebastianramli@rocketmail.com>
2024-06-12 12:32:58 +0700

    Added cart in catalogue

--- src/pages/catalogue/card/card.tsx
+++ src/pages/catalogue/card/card.tsx
@@ -1,4 +1,7 @@
 import css from './.module.scss'
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 interface _cardProps {
     imgPath: string;
@@ -15,13 +18,23 @@ const _card: React.FC<_cardProps> = ({imgPath, title, price}: _cardProps) => {
             // backgroundImage: imgPath,
         }
     }
-
+    const [count, setCount] = useState<number>(0);
+    function incrementCount(){
+        setCount(count + 1);
+        console.log(count);
+    }
     return (
         <div className={css.cont}>
             <div style={style.b_i}> {/* bg image here */} </div>
             <div>
                 <h5>{title}</h5>
-                <p>{`Rp. ${price.toLocaleString()}`}</p>
+                <div className={css.bot_card}>
+                    <p>{`Rp. ${price.toLocaleString()}`}</p>
+                    <div>
+                        <FontAwesomeIcon icon={faShoppingCart} onClick={incrementCount} />
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
